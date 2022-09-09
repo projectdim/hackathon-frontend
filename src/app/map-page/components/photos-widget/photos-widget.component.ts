c0c15f16db009d8c7b1ac8fe03273f01e4f5a943
Bartłomiej Bigda <bartlomiej.bigda@promity.pl>
2022-09-09 20:45:35 +0200

    select marker, display photos

--- src/app/map-page/components/photos-widget/photos-widget.component.ts
+++ src/app/map-page/components/photos-widget/photos-widget.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-photos-widget',
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotosWidgetComponent implements OnInit {
 
-  photos:Array<any> = [].constructor(10)
-
+  @Input()
+  photos?:Array<any> = [];
 
   constructor() { }
 
